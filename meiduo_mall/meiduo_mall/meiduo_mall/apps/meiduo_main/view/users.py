b1edf60c6c24531f4012af64eb52311cb87510e1
xwp <xwp_fullstack@163.com>
2020-08-28 21:26:20 +0800

    改写get_queryset方法

--- meiduo_mall/meiduo_mall/meiduo_mall/apps/meiduo_main/view/users.py
+++ meiduo_mall/meiduo_mall/meiduo_mall/apps/meiduo_main/view/users.py
@@ -10,7 +10,12 @@ from meiduo_main.utils import PageUnm
 
 class UserView(ListAPIView):
     parser_classes = [IsAdminUser]
-    queryset = User.objects.all()
+    # queryset = User.objects.all()
     serializer_class = UerShow
     # 指定使用的序列化器
     pagination_class = PageUnm
+    def get_queryset(self):
+        if self.request.query_params.get('keyword') == '':
+            return User.objects.all()
+        else:
+            return User.objects.filter(username__contains=self.request.query_params.get('keyword'))
