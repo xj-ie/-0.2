2fb8ec4fd76a884c5d8309a5816f28738a7e1fc5
xwp <xwp_fullstack@163.com>
2020-08-29 10:49:53 +0800

    用户新增接口的完成

--- meiduo_mall/meiduo_mall/meiduo_mall/apps/meiduo_main/verializer/users.py
+++ meiduo_mall/meiduo_mall/meiduo_mall/apps/meiduo_main/verializer/users.py
@@ -1,3 +1,5 @@
+import re
+
 from rest_framework import serializers
 
 from users.models import User
@@ -6,6 +8,29 @@ from users.models import User
 class UerShow(serializers.ModelSerializer):
     class Meta:
         model = User
-        # fields = ('id','username', 'email', 'mobile')
         # exclude =
-        fields = ('id', 'username', 'mobile', 'email')
+        fields = ('id', 'username', 'mobile', 'email', 'password')
+
+        extra_kwargs = {
+                            "username":{
+                                'max_length': 20,
+                                'min_length': 5
+                            },
+                            "password":{
+                                'max_length':20,
+                                'min_length':8,
+                                'write_only': True
+                            },
+
+                       }
+    def validate_mobile(self, attrs):
+        if not re.match(r'1[3-9]\d{9}',attrs):
+
+
+            raise serializers.ValidationError('手机号不正确')
+
+        return attrs
+    def create(self, validated_data):
+        # user = super().create(validated_data)
+        user = User.objects.create_user(**validated_data)
+        return user
--- meiduo_mall/meiduo_mall/meiduo_mall/apps/meiduo_main/view/users.py
+++ meiduo_mall/meiduo_mall/meiduo_mall/apps/meiduo_main/view/users.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView,CreateAPIView,ListCreateAPIView
 from rest_framework.permissions import IsAdminUser
 
 from users.models import User
@@ -8,14 +8,16 @@ from meiduo_main.verializer.users import UerShow
 from meiduo_main.utils import PageUnm
 
 
-class UserView(ListAPIView):
-    parser_classes = [IsAdminUser]
+class UserView(ListCreateAPIView):
+    # parser_classes = [IsAdminUser]
     # queryset = User.objects.all()
     serializer_class = UerShow
     # 指定使用的序列化器
     pagination_class = PageUnm
     def get_queryset(self):
-        if self.request.query_params.get('keyword') == '':
+        if self.request.query_params.get('keyword') == None:
             return User.objects.all()
         else:
             return User.objects.filter(username__contains=self.request.query_params.get('keyword'))
+    # def get_serializer(self, *args, **kwargs):
+    #     pass
